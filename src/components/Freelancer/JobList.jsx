5b1eed1aef819ba60b8aca189bc05dd016148dec
sosiabera <sosiabera32@gmail.com>
2024-06-14 08:32:26 -0300

    new

--- src/components/Freelancer/JobList.jsx
+++ src/components/Freelancer/JobList.jsx
@@ -8,11 +8,13 @@ import {
   faMinus,
   faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
+import { format } from "timeago.js";
 import "./css/joblist.css";
 
 export default function Joblist() {
   const [readData, setreadData] = useState([]);
   const [jobtype, setjobtype] = useState("");
+  const [jobtask, setjobtask] = useState("");
   const [serach, setsearch] = useState("");
   let defalutjobtype = ["onsite", "remote", "hybrid"];
   const [searchicon, setsearchicon] = useState("");
@@ -30,6 +32,15 @@ export default function Joblist() {
   };
   const handlejobtyped = () => {
     setjobtype("");
+  };
+
+  const handlejobtask = (type) => {
+    setjobtask(type);
+    console.log(jobtype);
+  };
+  
+  const handlejobtaskd = () => {
+    setjobtask("");
   };
 
   const handelsearch = (e) => {
@@ -50,7 +61,7 @@ export default function Joblist() {
         const response = await axios.get(
           "http://localhost:4000/post/readpost",
           {
-            params: { search: jobtype, serachtitle: serach },
+            params: { search: jobtype, serachtitle: serach, filter: jobtask },
           }
         );
 
@@ -63,7 +74,7 @@ export default function Joblist() {
       }
     };
     fetchData();
-  }, [jobtype, serach]);
+  }, [jobtype, serach,jobtask]);
 
   let navigate = useNavigate();
 
@@ -71,16 +82,86 @@ export default function Joblist() {
     navigate("Apply", { state: { postid: postid } });
   };
 
+
   return (
     <>
+      {jobtask === "" ? (
+        <>
+          <div
+          className=" allfilter"
+          onClick={() => {
+            handlejobtask("Job");
+          }}
+        >
+          <FontAwesomeIcon className="delete" icon={faPlus} />
+          Job
+        </div>
+        <div
+        className=" allfilter two"
+        onClick={() => {
+          handlejobtask("Task");
+        }}
+      >
+        <FontAwesomeIcon className="delete" icon={faPlus} />
+        Task
+      </div>
+</>
+      ) : (
+        <>
+        {jobtask === "Task" && (
+          <>
+          
+          <div
+          className=" allfilter"
+          onClick={() => {
+            handlejobtask("Job");
+          }}
+        >
+          <FontAwesomeIcon className="delete" icon={faPlus} />
+          Job
+        </div>
+          <div
+            className="allfilter two"
+            onClick={handlejobtaskd}
+          >
+            <FontAwesomeIcon className="delete" icon={faMinus} />
+            Task
+          </div>
+          </>
+        )}
+        {jobtask === "Job" && (
+          <>
+          <div
+            className="allfilter"
+            onClick={handlejobtaskd}
+          >
+            <FontAwesomeIcon className="delete" icon={faMinus} />
+            Job
+          </div>
+                 <div
+                 className=" allfilter two"
+                 onClick={() => {
+                   handlejobtask("Task");
+                 }}
+               >
+                 <FontAwesomeIcon className="delete" icon={faPlus} />
+                 Task
+               </div>
+               </>
+        )}
+      </>
+      )}
+
+
+
       <div className="jobparent">
         <div className={`serachparent`}>
           <FontAwesomeIcon
-            className={`search${searchicon} end-0`}
+            className={`search s${searchicon} end-0`}
             icon={faSearch}
           />
           <input
-            className={`another${searchicon} end-0`}
+            className={`another  end-0`}
             type="text"
             placeholder="Search Job"
             onChange={handelsearch}
@@ -132,28 +213,30 @@ export default function Joblist() {
             )}
           </div>
         </div>
+
         {isempty(readData) ? (
           <div>
             {readData.map((data) => (
               <>
-              <div onClick={() => handleclick(data._id)} className="postblock box">
-               
+                <div
+                  onClick={() => handleclick(data._id)}
+                  className={`postblock box ${data.urgency ? "urgent" : ""}`}
+                >
                   <div class="ribbon-2">{data.JobTask}</div>
-             
 
-            
-                <div>
-                  <h3 className="text">Job Type </h3>
-                  <p className="title">{data.Jobtype}</p>
+                  <div className="date">{format(data.PostedDate)} </div>
+
+                  <div>
+                    <h3 className="text">Job Type </h3>
+                    <p className="title">{data.Jobtype}</p>
+                  </div>
+                  <h3 className="text">Job Title </h3>
+                  <p className="title">{data.Jobtitle}</p>
+                  <h3 className="text">Location </h3>
+                  <p className="title">{data.location}</p>
                 </div>
-                <h3 className="text">Job Title </h3>
-                <p className="title">{data.Jobtitle}</p>
-                <h3 className="text">Location </h3>
-                <p className="title">{data.location}</p>
-               
-              </div>
-               <button className="btn-job">More Information</button>
-               </>
+                <button className="btn-job">More Information</button>
+              </>
             ))}
           </div>
         ) : (
